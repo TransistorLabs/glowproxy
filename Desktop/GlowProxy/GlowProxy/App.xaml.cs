a63bc6909c4d568df4ef19b03d208cadb4ff9819
Paul Trandem <ptrandem@gmail.com>
2014-06-05 22:16:29 -0500

    adding a timer to throttling service inputs... so much badness

--- Desktop/GlowProxy/GlowProxy/App.xaml.cs
+++ Desktop/GlowProxy/GlowProxy/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 
 using GlowProxyAPI.Net;
@@ -18,32 +19,62 @@ namespace GlowProxy
     /// </summary>
     public partial class App : Application
     {
+        private static Timer _sendTimer;
+        private bool _pingActivated = false;
+        private Color _color = new Color();
+        private Color _lastColor = new Color();
 
         private void InitApplication()
         {
             //initialize NotifyIcon
             Core.ApplicationIcon = (TaskbarIcon)FindResource("GlowProxyNotifyIcon");
             Core.Device.Open();
-            Core.Device.StartProcessingInput();
+
+            // Configure inputs from device
             Core.Device.OnClick = () =>
+            {
+                _pingActivated = true;
+            };
+
+            Core.Device.OnColorChange = (color) =>
+            {
+                _color = color;   
+            };
+
+            // Configure outputs to device
+            Core.ColorMessageClient.OnPing = () => Core.Device.BlinkWhite(4);
+            Core.ColorMessageClient.OnRemoteColorIndexChange = (colorString) => Core.Device.SetRemoteColor(new Color(colorString));
+            
+            _sendTimer = new Timer(50);
+            _sendTimer.Elapsed += sendTimer_Elapsed;
+            _sendTimer.Start();
+            
+            Core.Device.StartProcessingInput();
+
+
+        }
+
+        void sendTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _sendTimer.Stop();
+            if (_pingActivated)
             {
                 if (Core.ColorMessageClient.Connected)
                 {
                     Core.ColorMessageClient.SendPing();
                 }
-            };
+                _pingActivated = false;
+            }
 
-            Core.Device.OnColorChange = (color) =>
+            if (_color.ToString() != _lastColor.ToString())
             {
                 if (Core.ColorMessageClient.Connected)
                 {
-                    Core.ColorMessageClient.SendColorIndex(color.ToString());
+                    Core.ColorMessageClient.SendColorIndex(_color.ToString());
                 }
-            };
-
-            Core.ColorMessageClient.OnPing = () => Core.Device.BlinkWhite(4);
-            Core.ColorMessageClient.OnRemoteColorIndexChange = (colorString) => Core.Device.SetRemoteColor(new Color(colorString));
-
+                _lastColor = _color;
+            }
+            _sendTimer.Start();
         }
 
         private void App_OnStartup(object sender, StartupEventArgs e)
